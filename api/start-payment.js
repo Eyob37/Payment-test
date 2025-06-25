efa8250fbf801967ecde34a639d109695bdf23c8
Eyob <Eyobeb337@gmail.com>
2025-06-25 08:50:22 +0300

    Update start-payment.js

--- api/start-payment.js
+++ api/start-payment.js
@@ -2,7 +2,7 @@ export default function handler(req, res) {
   const orderId = 'order123';
   const amount = 100;
 
-  const telebirrURL = `https://touchpay.et/pay?amount=${amount}&order_id=${orderId}&callback_url=https://your-vercel-site.vercel.app/api/payment-callback`;
+  const telebirrURL = `https://touchpay.et/pay?amount=${amount}&order_id=${orderId}&callback_url=https://payment-test-lime.vercel.app/api/payment-callback`;
 
   res.status(200).json({ url: telebirrURL });
 }
