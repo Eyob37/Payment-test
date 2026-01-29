c1d15f1e022cd3bc6a0afec6c0789d62bbfe520d
Eyob <Eyobeb337@gmail.com>
2026-01-29 07:46:27 +0300

    Delete api directory

--- api/start-payment.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  const orderId = 'order123';
-  const amount = 100;
-
-  const telebirrURL = `https://touchpay.et/pay?amount=${amount}&order_id=${orderId}&callback_url=https://payment-test-lime.vercel.app/api/payment-callback`;
-
-  res.status(200).json({ url: telebirrURL });
-}
