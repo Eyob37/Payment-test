d6e8a819367d057149352521c1b7235b49c3bc18
Eyob <Eyobeb337@gmail.com>
2026-01-29 07:48:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-async function startPayment() {
-  const res = await fetch('/api/start-payment');
-  const data = await res.json();
-  if (data.url) {
-    window.location.href = data.url;
-  } else {
-    alert("Failed to start payment.");
-  }
-}
+hello
